6e178120933a2408368d8acac88e5fe086927a0e
Noah Zinn <nzinn08@gmail.com>
2019-08-26 18:50:01 -0400

    Add template for cplus plus header

---
+++ templates/cplusplus-header-template.hpp
@@ -0,0 +1,38 @@
+//Header Guards
+#ifndef SUBSYSTEM_DATA_STORAGE_H
+#define SUBSYSTEM_DATA_STORAGE_H
+
+//Include Files
+
+//C Interface
+#ifdef __cplusplus
+extern "C" {
+#endif
+//C Public Constants
+
+//C Public Variables
+
+//C Public Functions
+
+#ifdef __cplusplus
+}
+#endif //End C Interface
+
+//C++ Interface
+//Class Definitions
+class MY_CLASS
+{
+//Constructors
+
+//Public Functions
+
+//Public Constants
+
+//Public Variable
+
+//Private Variables
+
+//Private Functions
+};
+
+#endif //End Header Guard
