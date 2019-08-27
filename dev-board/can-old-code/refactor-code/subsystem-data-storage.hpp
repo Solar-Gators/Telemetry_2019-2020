2ce61075eb5390166b1cadd0fdf244aaa4e08394
Noah Zinn <nzinn08@gmail.com>
2019-08-26 20:19:26 -0400

    Added template to refactoring code

---
+++ dev-board/can-old-code/refactor-code/subsystem-data-storage.hpp
@@ -0,0 +1,49 @@
+//Header Guards
+#ifndef SUBSYSTEM_DATA_STORAGE_H_
+#define SUBSYSTEM_DATA_STORAGE_H_
+
+//Include Files
+
+//C Interface
+#ifdef __cplusplus
+extern "C" {
+#endif
+//C Public Constants
+/*This is an example description for constants and variables. Delete this if it is unused here and copy and paste it to where it is needed. */
+/**
+ * @brief This is a brief description
+ */
+
+//C Public Variables
+
+//C Public Function Prototypes
+/*This is an example description for function prototypes. Delete this if it is unused here and copy and paste it to where it is needed. */
+/**
+ * @brief This is a brief description
+ * @param Input_Param_Name - Description of input parameter
+ * @ret Description of return value
+ */
+
+
+#ifdef __cplusplus
+}
+#endif //End C Interface
+
+//C++ Interface
+//Class Definitions
+class MY_CLASS
+{
+//Constructors
+
+//Public Function Prototypes
+
+//Public Constants
+
+//Public Variable
+
+//Private Variables
+
+//Private Function Prototypes
+};
+
+#endif //End Header Guard
