b2cad9692c6af33592c30ead6203ecaa941e4cf6
Noah Zinn <nzinn08@gmail.com>
2019-09-09 13:36:27 -0400

    Started adding module code

---
+++ dev-board/can-old-code/refactor-code/subystem-data-module.hpp
@@ -0,0 +1,56 @@
+//File Name: subsystem-data-module.hpp
+//Description: This is what each subsystem will inherit from for storage
+
+//Header Guards
+#ifndef SUBSYSTEM_DATA_MODULE_H_
+#define SUBSYSTEM_DATA_MODULE_H_
+
+//Include Files
+
+//C Interface
+#ifdef __cplusplus
+extern "C" {
+#endif
+//C Public Constants
+/*This is an example description for constants and variables. Delete this if it is unused here and copy and paste it to where it is needed. */
+/**
+ * @brief This is a brief description
+ */
+
+//C Public Variables
+
+//C Public Function Prototypes
+/*This is an example description for function prototypes. Delete this if it is unused here and copy and paste it to where it is needed. */
+/**
+ * @brief This is a brief description
+ * @param Input_Param_Name - Description of input parameter
+ * @ret Description of return value
+ */
+
+
+#ifdef __cplusplus
+}
+#endif //End C Interface
+
+//C++ Interface
+//Class Definitions
+class SUBSYSTEM_DATA_MODULE 
+{
+public:
+//Constructors
+//This class is only for inheritance so delete all public constructors
+SUBSYSTEM_DATA_MODULE() = delete;
+SUBSYSTEM_DATA_MODULE(SUBSYSTEM_DATA_MODULE&) = delete;
+
+//Public Function Prototypes
+
+//Public Constants
+
+//Public Variable
+private:
+//Private Variables
+
+//Private Function Prototypes
+};
+
+#endif //End Header Guard
