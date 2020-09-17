5789d89a4eb95f968e04325f44acd6c506dd91c6
John Carr <johnjcarr17@gmail.com>
2020-09-17 18:19:15 -0400

    Pushing this commit wont work

--- dev-board/Inc/stm32f0xx_it.h
+++ dev-board/Inc/stm32f0xx_it.h
@@ -49,9 +49,7 @@
 /* Exported functions prototypes ---------------------------------------------*/
 void NMI_Handler(void);
 void HardFault_Handler(void);
-void SVC_Handler(void);
-void PendSV_Handler(void);
-void SysTick_Handler(void);
+void TIM1_BRK_UP_TRG_COM_IRQHandler(void);
 void USART1_IRQHandler(void);
 void CEC_CAN_IRQHandler(void);
 /* USER CODE BEGIN EFP */
